51b690a8a454dab4b34a777ec417502993cfc054
Sharona Young <youngs_e@hotmail.com>
2017-12-11 08:34:35 -0500

    updated js and css files. still trying to debug issues so that results are displayed on html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // VARIABLES
-var targetNumber = Math.floor((Math.random() * 100) + 20); // random number generator for target score, 20-100
+var targetNumber = Math.floor((Math.random() * 100 ) + 19); // random number generator for target score, 20-100
 var counter = 0; // counter for tracking total score
 var win = 0;
 var lose = 0;
@@ -8,27 +8,67 @@ var lose = 0;
 $("#wins").text(win);
 $("#losses").text(lose);
 
-// id for crystal images
-          
-// link the targetNum and targetNumber
+        
+// link the targetNum and targetNumber for showing results on html page. 
 $("#targetNum").text(targetNumber);
 
-$("#guesses").text(counter);
+//link counter to guesses on html page
+$("#guesses").text(counter); //*** UPDATING IN CONSOLE, BUT NOT UPDATING IN HTML ***
 
-// randon number generator for jewels
-var pink= Math.floor((Math.random() * 15) + 1);
+// randon number generator for jewels, any number 1-12
+var pink= Math.floor((Math.random() * 12) + 1);
+var yellow= Math.floor((Math.random() * 12) + 1);
+var green= Math.floor((Math.random() * 12) + 1);
+var purple= Math.floor((Math.random() * 12) + 1);
 
 //random number for each crystal
+$("#yellow").on("click", function() {
+  (counter += (yellow));
+  console.log(counter, yellow); score()
+});
 $("#pink").on("click", function() {
-  (counter += (pink));
-  console.log(counter);
-
-// logic for comparing score to target
-  if (counter === targetNumber){
-  alert("Awesome, you win"); win++;
-} else if (counter >= targetNumber){ 
-  alert("You lose!!"); lose++;
-}
+    (counter += (pink));
+    console.log(counter, pink); score()
+});
+$("#purple").on("click", function() {
+  (counter += (purple));
+  console.log(counter, purple); score()
+});
+$("#green").on("click", function() {
+    (counter += (green));
+    console.log(counter, green); score()
 });
 
 
+
+// logic for comparing score to target
+function score() { 
+  if (counter === targetNumber){ 
+  alert("Awesome, you win"); win ++; 
+  reset();
+}
+  else if (counter >= targetNumber){
+  lose ++; alert("You lose!!"); }
+  // reset();
+  
+}
+  console.log(lose); console.log(win); //console log to see if score is changing 
+  //***NOT CHANGING***
+
+// resets game 
+function reset () {
+  var targetNumber = Math.floor((Math.random() * 100 ) + 19);
+  pink= Math.floor((Math.random() * 12) + 1);
+  yellow= Math.floor((Math.random() * 12) + 1);
+  green= Math.floor((Math.random() * 12) + 1);
+  purple= Math.floor((Math.random() * 12) + 1);
+  counter = 0;
+  $("#guesses").text(counter);
+}
+
+// Set the inner HTML contents of #psychic div to html string
+// document.querySelector("#crystal").innerHTML = html;
+
+
+// function updates() {
+//   return counter
